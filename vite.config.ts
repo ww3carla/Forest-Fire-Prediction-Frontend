05be576974bd32ceff00bda66e1d26f59a8452b7
Carla Bozintan <carlabozintan3@gmail.com>
2026-02-17 16:13:26 +0200

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-
-  base: mode === "production" ? "/fire_forest_prediction_frontend/" : "/",
+  base: mode === "production" ? "/Forest-Fire-Prediction-Frontend/" : "/",
 
   server: {
     host: "::",
