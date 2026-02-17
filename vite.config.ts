d3ca77e9e8b3dc16a24d804c5184a5d27ea2f855
Carla Bozîntan <151360298+ww3carla@users.noreply.github.com>
2026-02-17 15:47:01 +0200

    Refactor Vite config for frontend deployment
    Removed server configuration and componentTagger plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+export default defineConfig({
+  base: "/fire_forest_prediction_frontend/",
+
+  plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
